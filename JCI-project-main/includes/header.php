10ca4668da49c04152150e5c9dc12d38efd46292
MoonCat-boop <mooncat4550@gmail.com>
2025-09-14 15:38:08 -0400

    Add files via upload

---
+++ JCI-project-main/includes/header.php
@@ -0,0 +1,47 @@
+<!-- includes/header.php -->
+  <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<header class="jci-header" id="site-header">
+  <div class="jci-header-top">
+    <img src="images/JCI Logo.png" alt="JCI Ottawa Logo" class="logo">
+    <div class="jci-buttons">
+      <a href="https://jci.cc/web/#/auth/sign-in1" class="btn login">Login</a>
+      <a href="https://app.glueup.com/org/jciottawa" class="btn join">Become a member</a>
+    </div>
+  </div>
+  <nav class="jci-nav">
+    <ul class="main-nav">
+    <li><a href="index.php">Home</a></li>
+    <li class="dropdown">
+      <a href="#">About Us</a>
+      <ul class="submenu">
+        <li><a href="about.php">About JCI</a></li>
+        <li><a href="team.php">Our Team</a></li>
+        
+      </ul>
+    </li>
+    <li class="dropdown">
+      <a href="#">Activities</a>
+      <ul class="submenu">
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Workshops</a></li>
+        <li><a href="events.php">Events</a></li>
+        <li><a href="#">Conferences</a></li>
+      </ul>
+    </li>
+    
+    <li><a href="contact.php">Contact</a></li>
+    <!-- <a href="#"><span class="search-icon">&#128269;</span></a> -->
+     </ul>
+  </nav>
+</header>
+</body>
+</html>
